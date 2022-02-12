9abdd14c0af05764e9171d6a2eabe9166b717a98
iamraihan <trueraihan@gmail.com>
2022-02-12 23:41:34 +0600

    function short and update total balance

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
+function updateCaseNumbers(isIncreasing) {
+    const caseInput = document.getElementById('case-number')
+    let caseNumber = caseInput.value
+    if (isIncreasing == true) {
+        caseNumber = parseInt(caseNumber) + 1
+    }
+    else if (caseNumber > 0) {
+        caseNumber = parseInt(caseNumber) - 1
+    }
+    //update case total
+    let caseTotal = document.getElementById('case-total')
+    caseTotal.innerText = caseNumber * 59
+    caseInput.value = caseNumber
+}
+
 //case plus 
 document.getElementById('case-plus').addEventListener('click', function () {
-    const caseInput = document.getElementById('case-number')
-    const caseNumber = caseInput.value
-    caseInput.value = parseInt(caseNumber) + 1
+    updateCaseNumbers(true)
 })
 
 // case minus 
 document.getElementById('case-minus').addEventListener('click', function () {
-    const caseInput = document.getElementById('case-number')
-    const caseNumber = caseInput.value
-    caseInput.value = parseInt(caseNumber) - 1
+    updateCaseNumbers(false)
 })
