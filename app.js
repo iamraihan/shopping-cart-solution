88760f3f658590d3089230e3b881c48df31e6a31
iamraihan <trueraihan@gmail.com>
2022-02-12 22:50:01 +0600

    minus aded

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+//case plus 
 document.getElementById('case-plus').addEventListener('click', function () {
     const caseInput = document.getElementById('case-number')
     const caseNumber = caseInput.value
     caseInput.value = parseInt(caseNumber) + 1
+})
+
+// case minus 
+document.getElementById('case-minus').addEventListener('click', function () {
+    const caseInput = document.getElementById('case-number')
+    const caseNumber = caseInput.value
+    caseInput.value = parseInt(caseNumber) - 1
 })
