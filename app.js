e00d493149ff66008cee5b9c49c8b4c735b5ed22
iamraihan <trueraihan@gmail.com>
2022-02-13 11:45:06 +0600

    sub total tax total price aded

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ function updateProductNumbers(product, price, isIncreasing) {
     //update total
     let productTotal = document.getElementById(product + '-total')
     productTotal.innerText = productNumber * price
+
+    // calculate Total
+    calculateTotal()
 }
 //phone plus
 document.getElementById('phone-plus').addEventListener('click', function () {
@@ -30,3 +33,23 @@ document.getElementById('case-plus').addEventListener('click', function () {
 document.getElementById('case-minus').addEventListener('click', function () {
     updateProductNumbers('case', 59, false)
 })
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number')
+    const productNumber = parseInt(productInput.value)
+    return productNumber
+}
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219
+    const caseTotal = getInputValue('case') * 59
+    const subTotal = phoneTotal + caseTotal
+    const tax = subTotal / 10
+    const totalPrice = subTotal + tax
+
+    // update on html 
+    document.getElementById('sub-total').innerText = subTotal
+    document.getElementById('tax-amount').innerText = tax
+    document.getElementById('total-price').innerText = totalPrice
+
+    console.log(subTotal);
+}
